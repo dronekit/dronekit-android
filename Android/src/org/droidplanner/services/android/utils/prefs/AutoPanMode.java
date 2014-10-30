d0fe1d233ef84deeb6fb20231f55edfbc59da5a0
ne0fhyk <fhuyakou@gmail.com>
2014-10-30 15:06:45 -0700

    removed ui related code.

---
+++ Android/src/org/droidplanner/services/android/utils/prefs/AutoPanMode.java
@@ -0,0 +1,10 @@
+package org.droidplanner.services.android.utils.prefs;
+
+/**
+ * Map auto pan target types.
+ */
+public enum AutoPanMode {
+	USER, DRONE, DISABLED;
+
+	public static final String PREF_KEY = "pref_auto_pan_mode";
+}
