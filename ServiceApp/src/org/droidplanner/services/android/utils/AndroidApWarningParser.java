65356112f65d0e86dacc44cd861060060a879ff7
ne0fhyk <fhuyakou@gmail.com>
2014-12-24 09:18:40 -0800

    completed implementation of the autopilot warnings parser.

--- ServiceApp/src/org/droidplanner/services/android/utils/AndroidApWarningParser.java
+++ ServiceApp/src/org/droidplanner/services/android/utils/AndroidApWarningParser.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import org.droidplanner.core.drone.variables.Type;
 import org.droidplanner.core.model.AutopilotWarningParser;
 import org.droidplanner.core.model.Drone;
+import org.droidplanner.services.android.R;
 
 /**
  * Created by fhuya on 12/16/14.
@@ -34,30 +35,45 @@ public class AndroidApWarningParser implements AutopilotWarningParser {
         }
     }
 
+    /**
+     * Maps the ArduRover warnings set to the 3DR Services warnings set.
+     * @param warning warning originating from the ArduRover autopilot
+     * @return equivalent 3DR Services warning
+     */
     private String parseRoverWarning(String warning) {
-        //TODO: complete mapping from ArduRover warnings to DP warnings.
-
-        switch(warning){
-            default:
-                return warning;
-        }
-    }
-
-    private String parsePlaneWarning(String warning) {
-        //TODO: complete mapping from ArduPlane warnings to DP warnings.
-
-        switch(warning){
-            default:
-                return warning;
-        }
-    }
-
-    private String parseCopterWarning(String warning) {
-        //TODO: complete mapping from ArduCopter warnings to DP warning
-
         switch(warning){
             case "Arm: Thr below FS":
-                return "Arm: Throttle below Failsafe";
+                return context.getString(R.string.warning_arm_thr_below_fs);
+
+            default:
+                return warning;
+        }
+    }
+
+    /**
+     * Maps the ArduPlane warnings set to the 3DR Services warnings set.
+     * @param warning warning originating from the ArduPlane autopilot
+     * @return equivalent 3DR Services warning
+     */
+    private String parsePlaneWarning(String warning) {
+        switch(warning){
+            case "Arm: Thr below FS":
+                return context.getString(R.string.warning_arm_thr_below_fs);
+
+            default:
+                return warning;
+        }
+    }
+
+    /**
+     * Maps the ArduCopter warnings set to the 3DR Services warnings set.
+     * @param warning warning originating from the ArduCopter autopilot
+     * @return equivalent 3DR Services warning
+     */
+    private String parseCopterWarning(String warning) {
+        switch(warning){
+            case "Arm: Thr below FS":
+                return context.getString(R.string.warning_arm_thr_below_fs);
 
             default:
                 return warning;
