544665f0a1fff56c42d733149a811650b9bb350f
Phu Nguyen <phmagic@gmail.com>
2015-01-21 15:29:48 -0800

    - moved build into output

--- doc/conf.py
+++ doc/conf.py
@@ -68,7 +68,7 @@ release = '1.1.6beta'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['build']
+exclude_patterns = ['output']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
--- doc/make.bat
+++ doc/make.bat
@@ -5,7 +5,7 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set BUILDDIR=build
+set BUILDDIR=output
 set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% .
 set I18NSPHINXOPTS=%SPHINXOPTS% .
 if NOT "%PAPER%" == "" (
