5dbe0ad8c89b6d9dbd7407fdd68b4752271442cf
Diego Medina <djmedina@3drobotics.com>
2015-03-04 15:51:34 -0800

    Adding SetServo non-mission command.

--- ClientLib/mobile/src/main/java/com/o3dr/android/client/apis/drone/ExperimentalApi.java
+++ ClientLib/mobile/src/main/java/com/o3dr/android/client/apis/drone/ExperimentalApi.java
@@ -14,6 +14,10 @@ import static com.o3dr.services.android.lib.drone.action.ExperimentalActions.EXT
 import static com.o3dr.services.android.lib.drone.action.ExperimentalActions.EXTRA_IS_RELAY_ON;
 import static com.o3dr.services.android.lib.drone.action.ExperimentalActions.EXTRA_MAVLINK_MESSAGE;
 import static com.o3dr.services.android.lib.drone.action.ExperimentalActions.EXTRA_RELAY_NUMBER;
+import static com.o3dr.services.android.lib.drone.action.ExperimentalActions.ACTION_SET_SERVO;
+import static com.o3dr.services.android.lib.drone.action.ExperimentalActions.EXTRA_SERVO_CHANNEL;
+import static com.o3dr.services.android.lib.drone.action.ExperimentalActions.EXTRA_SERVO_PWM;
+
 
 /**
  * Contains drone commands with no defined interaction model yet.
@@ -35,11 +39,11 @@ public class ExperimentalApi {
      * This is an advanced/low-level method to send raw mavlink to the vehicle.
      * <p/>
      * This method is included as an ‘escape hatch’ to allow developers to make progress if we’ve
-     * somehow missed providing some essentential operation in the rest of this API. Callers do
+     * somehow missed providing some essential operation in the rest of this API. Callers do
      * not need to populate sysId/componentId/crc in the packet, this method will take care of that
      * before sending.
      * <p/>
-     * If you find yourself needing to use this mathod please contact the drone-platform google
+     * If you find yourself needing to use this method please contact the drone-platform google
      * group and we’ll see if we can support the operation you needed in some future revision of
      * the API.
      *
@@ -64,5 +68,19 @@ public class ExperimentalApi {
         params.putInt(EXTRA_RELAY_NUMBER, relayNumber);
         params.putBoolean(EXTRA_IS_RELAY_ON, enabled);
         drone.performAsyncAction(new Action(ACTION_SET_RELAY, params));
+    }
+
+    /**
+     * Move a servo to a particular pwm value
+     *
+     * @param drone       target vehicle
+     * @param channel     the output channel the servo is attached to
+     * @param pwm         PWM value to output to the servo. Servo’s generally accept pwm values between 1000 and 2000
+     */
+    public static void setServo(Drone drone, int channel, int pwm) {
+        Bundle params = new Bundle(2);
+        params.putInt(EXTRA_SERVO_CHANNEL, channel);
+        params.putInt(EXTRA_SERVO_PWM, pwm);
+        drone.performAsyncAction(new Action(ACTION_SET_SERVO, params));
     }
 }
