687fb668ebf4727aad1b7a27c9195368e6b8051b
ne0fhyk <fhuyakou@gmail.com>
2015-03-22 14:14:41 -0400

    set the basic test to run as a roboelectric powered junit test.

--- ServiceApp/test/java/org/droidplanner/services/android/BasicTest.java
+++ ServiceApp/test/java/org/droidplanner/services/android/BasicTest.java
@@ -1,10 +1,9 @@
 package org.droidplanner.services.android;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
-import android.test.AndroidTestCase;
-import android.test.suitebuilder.annotation.SmallTest;
 
 import com.MAVLink.MAVLinkPacket;
 import com.MAVLink.Messages.MAVLinkMessage;
@@ -12,21 +11,31 @@ import com.MAVLink.common.msg_command_long;
 import com.MAVLink.enums.MAV_CMD;
 import com.o3dr.services.android.lib.drone.connection.ConnectionParameter;
 
+import junit.framework.Assert;
+
 import org.droidplanner.core.MAVLink.MAVLinkStreams;
 import org.droidplanner.core.MAVLink.MavLinkArm;
 import org.droidplanner.core.drone.DroneImpl;
 import org.droidplanner.core.drone.DroneInterfaces;
 import org.droidplanner.core.model.Drone;
 import org.droidplanner.services.android.communication.service.MAVLinkClient;
+import org.droidplanner.services.android.mock.MockMavLinkServiceAPI;
 import org.droidplanner.services.android.utils.AndroidApWarningParser;
 import org.droidplanner.services.android.utils.prefs.DroidPlannerPrefs;
-import org.droidplanner.services.android.mock.MockMavLinkServiceAPI;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 /**
  * Created by djmedina on 3/5/15.
  * This is a simple test case.
  */
-public class BasicTest extends AndroidTestCase {
+@RunWith(RobolectricTestRunner.class)
+@Config(emulateSdk = 18)
+public class BasicTest {
 
     private Drone drone;
     private MockMavLinkServiceAPI mavlinkApi;
@@ -52,24 +61,29 @@ public class BasicTest extends AndroidTestCase {
 
     private final MAVLinkStreams.MavlinkInputStream inputStreamListener = new MAVLinkStreams.MavlinkInputStream() {
         @Override
-        public void notifyStartingConnection() {}
+        public void notifyStartingConnection() {
+        }
 
         @Override
-        public void notifyConnected() {}
+        public void notifyConnected() {
+        }
 
         @Override
-        public void notifyDisconnected() {}
+        public void notifyDisconnected() {
+        }
 
         @Override
-        public void notifyReceivedData(MAVLinkPacket packet) {}
+        public void notifyReceivedData(MAVLinkPacket packet) {
+        }
 
         @Override
-        public void onStreamError(String errorMsg) {}
+        public void onStreamError(String errorMsg) {
+        }
     };
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
+    @Before
+    public void setUp() throws Exception {
+        final Context context = Robolectric.getShadowApplication().getApplicationContext();
 
         ConnectionParameter connParams = new ConnectionParameter(0, new Bundle(), null);
         mavlinkApi = new MockMavLinkServiceAPI();
@@ -79,10 +93,10 @@ public class BasicTest extends AndroidTestCase {
                 return SystemClock.elapsedRealtime();
             }
         };
-        DroidPlannerPrefs dpPrefs = new DroidPlannerPrefs(this.getContext());
-        MAVLinkClient mavClient = new MAVLinkClient(this.getContext(), inputStreamListener, connParams, mavlinkApi);
+        DroidPlannerPrefs dpPrefs = new DroidPlannerPrefs(context);
+        MAVLinkClient mavClient = new MAVLinkClient(context, inputStreamListener, connParams, mavlinkApi);
 
-        drone = new DroneImpl(mavClient, clock, dpHandler, dpPrefs, new AndroidApWarningParser(this.getContext()));
+        drone = new DroneImpl(mavClient, clock, dpHandler, dpPrefs, new AndroidApWarningParser(context));
     }
 
     /**
@@ -91,25 +105,25 @@ public class BasicTest extends AndroidTestCase {
      * explain any and all failures in other tests.  This is not guaranteed
      * to run before other tests, as junit uses reflection to find the tests.
      */
-    @SmallTest
+    @Test
     public void testPreconditions() {
     }
 
     /**
      * Basic MAVLink message test
      */
-    @SmallTest
+    @Test
     public void testArm() {
         MavLinkArm.sendArmMessage(drone, true);
         MAVLinkPacket data = mavlinkApi.getData();
-        assertTrue(data != null);
+        Assert.assertTrue(data != null);
 
         //Unpack the message into the right MAVLink message type
         MAVLinkMessage msg = data.unpack();
         msg_command_long longMsg = (msg_command_long) msg;
 
         //Validate the message
-        assertEquals(MAV_CMD.MAV_CMD_COMPONENT_ARM_DISARM, longMsg.command);
-        assertEquals(1f, longMsg.param1);
+        Assert.assertEquals(MAV_CMD.MAV_CMD_COMPONENT_ARM_DISARM, longMsg.command);
+        Assert.assertEquals(1f, longMsg.param1);
     }
 }
