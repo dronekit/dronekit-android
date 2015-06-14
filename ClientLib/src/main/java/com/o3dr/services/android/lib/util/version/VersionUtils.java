2207f894d7b7b42dea51e33c61bed4d980fc41a6
ne0fhyk <fhuyakou@gmail.com>
2015-06-13 18:25:23 -0700

    updated client lib version.

--- ClientLib/src/main/java/com/o3dr/services/android/lib/util/version/VersionUtils.java
+++ ClientLib/src/main/java/com/o3dr/services/android/lib/util/version/VersionUtils.java
@@ -8,7 +8,7 @@ import android.content.pm.PackageManager;
  */
 public class VersionUtils {
 
-    public static final int LIB_VERSION = 20214;
+    public static final int LIB_VERSION = 20215;
 
     public static int getVersion(Context context){
         try {
