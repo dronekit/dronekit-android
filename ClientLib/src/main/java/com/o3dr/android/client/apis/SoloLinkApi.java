080505a34d48b2959acd26312bde5cb41dd6ba2f
ne0fhyk <fhuyakou@gmail.com>
2015-07-13 09:47:42 -0700

    Added wifi settings update, button settings update, and sololink message dispatch functionality to the SoloLink api.

--- ClientLib/src/main/java/com/o3dr/android/client/apis/SoloLinkApi.java
+++ ClientLib/src/main/java/com/o3dr/android/client/apis/SoloLinkApi.java
@@ -1,6 +1,12 @@
 package com.o3dr.android.client.apis;
 
+import android.os.Bundle;
+
 import com.o3dr.android.client.Drone;
+import com.o3dr.services.android.lib.drone.companion.solo.tlv.SoloButtonSettingSetter;
+import com.o3dr.services.android.lib.drone.companion.solo.tlv.TLVPacket;
+import com.o3dr.services.android.lib.model.AbstractCommandListener;
+import com.o3dr.services.android.lib.model.action.Action;
 
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -34,7 +40,38 @@ public class SoloLinkApi implements Api {
         this.drone = drone;
     }
 
-    public void updateWifiSettings(String wifiSsid, String wifiPassword){
+    /**
+     * Updates the wifi settings for the solo vehicle.
+     * @param wifiSsid Updated wifi ssid
+     * @param wifiPassword Updated wifi password
+     * @param listener Register a callback to receive update of the command execution status.
+     */
+    public void updateWifiSettings(String wifiSsid, String wifiPassword, AbstractCommandListener listener){
+        Bundle params = new Bundle();
+        params.putString(EXTRA_WIFI_SSID, wifiSsid);
+        params.putString(EXTRA_WIFI_PASSWORD, wifiPassword);
+        drone.performAsyncActionOnDroneThread(new Action(ACTION_UPDATE_WIFI_SETTINGS, params), listener);
+    }
 
+    /**
+     * Updates the button settings for the solo vehicle.
+     * @param buttonSettings Updated button settings.
+     * @param listener Register a callback to receive update of the command execution status.
+     */
+    public void updateButtonSettings(SoloButtonSettingSetter buttonSettings, AbstractCommandListener listener){
+        Bundle params = new Bundle();
+        params.putParcelable(EXTRA_BUTTON_SETTINGS, buttonSettings);
+        drone.performAsyncActionOnDroneThread(new Action(ACTION_UPDATE_BUTTON_SETTINGS, params), listener);
+    }
+
+    /**
+     * Sends a message to the solo vehicle.
+     * @param messagePacket TLV message data.
+     * @param listener Register a callback to receive update of the command execution status.
+     */
+    public void sendMessage(TLVPacket messagePacket, AbstractCommandListener listener){
+        Bundle params = new Bundle();
+        params.putParcelable(EXTRA_MESSAGE_DATA, messagePacket);
+        drone.performAsyncActionOnDroneThread(new Action(ACTION_SEND_MESSAGE, params), listener);
     }
 }
