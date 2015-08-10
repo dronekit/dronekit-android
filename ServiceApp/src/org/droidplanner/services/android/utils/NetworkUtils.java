0fb5bbb9cca0379838909ca4afca4c7559d7b4b9
Diego Medina <djmedina@3drobotics.com>
2015-08-10 15:40:23 -0700

    Modify get connections.

--- ServiceApp/src/org/droidplanner/services/android/utils/NetworkUtils.java
+++ ServiceApp/src/org/droidplanner/services/android/utils/NetworkUtils.java
@@ -6,15 +6,20 @@ import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 
+import org.droidplanner.services.android.BuildConfig;
 import org.droidplanner.services.android.core.drone.companion.solo.SoloComp;
 
 /**
  * Created by Fredia Huya-Kouadio on 5/11/15.
  */
 public class NetworkUtils {
-
+    /**
+     * Is internet connection available. This method also returns true for the SITL build type
+     * @param context
+     * @return Internet connection availability.
+     */
     public static boolean isNetworkAvailable(Context context) {
-        if (isOnSololinkNetwork(context))
+        if (!BuildConfig.SITL_DEBUG && isOnSololinkNetwork(context))
             return false;
 
         ConnectivityManager connectivityManager = (ConnectivityManager) context
@@ -32,6 +37,8 @@ public class NetworkUtils {
     }
 
     public static boolean isOnSololinkNetwork(Context context) {
+        if (BuildConfig.SITL_DEBUG)
+            return true;
 
         final String connectedSSID = getCurrentWifiLink(context);
         return connectedSSID != null && connectedSSID.startsWith(SoloComp.SOLO_LINK_WIFI_PREFIX);
