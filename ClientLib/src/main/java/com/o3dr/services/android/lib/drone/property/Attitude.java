38947f78d157d211257bb52f8897dbbf466724f6
ne0fhyk <fhuyakou@gmail.com>
2015-10-13 18:36:36 -0700

    Updated `Attitude` javadocs.

--- ClientLib/src/main/java/com/o3dr/services/android/lib/drone/property/Attitude.java
+++ ClientLib/src/main/java/com/o3dr/services/android/lib/drone/property/Attitude.java
@@ -49,50 +49,92 @@ public class Attitude implements DroneAttribute {
         this.yawSpeed = yawSpeed;
     }
 
+    /**
+     * Updates the roll angle
+     * @param roll Roll angle (deg, -180..+180)
+     */
     public void setRoll(double roll) {
         this.roll = roll;
     }
 
+    /**
+     * Updates the pitch angle
+     * @param pitch Pitch angle (deg, -180..+180)
+     */
     public void setPitch(double pitch) {
         this.pitch = pitch;
     }
 
+    /**
+     * Updates the yaw angle
+     * @param yaw Yaw angle (deg, -180..+180)
+     */
     public void setYaw(double yaw) {
         this.yaw = yaw;
     }
 
+    /**
+     * @return Vehicle roll angle (deg, -180..+180)
+     */
     public double getRoll() {
         return roll;
     }
 
+    /**
+     * @return Vehicle pitch angle (deg, -180 to 180)
+     */
     public double getPitch() {
         return pitch;
     }
 
+    /**
+     * @return Vehicle yaw angle (deg, -180 to 180)
+     */
     public double getYaw() {
         return yaw;
     }
 
+    /**
+     * @return Vehicle pitch angular speed (deg / s)
+     */
     public float getPitchSpeed() {
         return pitchSpeed;
     }
 
+    /**
+     * Updates the pitch angular speed
+     * @param pitchSpeed Pitch angular speed (deg/s)
+     */
     public void setPitchSpeed(float pitchSpeed) {
         this.pitchSpeed = pitchSpeed;
     }
 
+    /**
+     * @return Vehicle roll angular speed (deg/s)
+     */
     public float getRollSpeed() {
         return rollSpeed;
     }
 
+    /**
+     * Updates the roll angular speed
+     * @param rollSpeed Roll angular speed (deg/s)
+     */
     public void setRollSpeed(float rollSpeed) {
         this.rollSpeed = rollSpeed;
     }
 
+    /**
+     * @return Vehicle yaw angular speed (deg/ s)
+     */
     public float getYawSpeed() {
         return yawSpeed;
     }
 
+    /**
+     * Updates the yaw angular speed
+     * @param yawSpeed Yaw angular speed (deg/s)
+     */
     public void setYawSpeed(float yawSpeed) {
         this.yawSpeed = yawSpeed;
     }
