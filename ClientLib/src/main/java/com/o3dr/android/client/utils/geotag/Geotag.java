e50d57c8405c0d46e2133dedc48fb89153b85e1a
Chavi Weingarten <chavi@3drobotics.com>
2015-10-15 13:52:19 -0700

    Cleaned up code

--- ClientLib/src/main/java/com/o3dr/android/client/utils/geotag/Geotag.java
+++ ClientLib/src/main/java/com/o3dr/android/client/utils/geotag/Geotag.java
@@ -1,4 +1,4 @@
-package com.o3dr.android.client.utils;
+package com.o3dr.android.client.utils.geotag;
 
 import android.content.Context;
 import android.media.ExifInterface;
@@ -6,7 +6,6 @@ import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.StatFs;
-import android.util.Log;
 
 import com.MAVLink.ardupilotmega.msg_camera_feedback;
 import com.o3dr.android.client.data.tlog.TLogParser;
@@ -21,6 +20,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -28,7 +28,8 @@ import java.util.concurrent.Executors;
  * Geotag images based on camera mavlink messages.
  */
 public class Geotag {
-    private static final String LOG_TAG = Geotag.class.getSimpleName();
+    private Geotag() {
+    }
 
     private static ExecutorService getInstance() {
         return InitializeExecutorService.executorService;
@@ -39,12 +40,13 @@ public class Geotag {
     }
 
     /**
+     * Asynchronous method to geotag a list of images using a list of Events as coordinate data.
      *
-     * @param context {@link Context}
-     * @param handler {@link Handler} Handler to specify what thread to callback on. This cannot be null.
-     * @param events {@link List<com.o3dr.android.client.data.tlog.TLogParser.Event>} list of events to geotag photos.
-     * @param photos {@link List<File>} list of files of photos to geotag.
-     * @param callback {@link com.o3dr.android.client.utils.Geotag.GeoTagCallback} callback to send results to.
+     * @param context  {@link Context}
+     * @param handler  {@link Handler} Handler to specify what thread to callback on. This cannot be null.
+     * @param events   {@link List<com.o3dr.android.client.data.tlog.TLogParser.Event>} list of events to geotag photos.
+     * @param photos   {@link List<File>} list of files of photos to geotag.
+     * @param callback {@link Geotag.GeoTagCallback} callback to send results to.
      */
     public static void geoTagImagesAsync(final Context context, final Handler handler, final List<TLogParser.Event> events,
                                          final ArrayList<File> photos, final GeoTagCallback callback) {
@@ -56,42 +58,47 @@ public class Geotag {
         getInstance().execute(new Runnable() {
             @Override
             public void run() {
-                File dirs[] = context.getExternalFilesDirs(null);
-                File saveDir = context.getExternalFilesDir(null);
-                for (File dir : dirs) {
-                    if (Environment.isExternalStorageRemovable(dir)) {
-                        saveDir = dir;
-                        break;
-                    }
-                }
+                File saveDir = getSaveDir(context);
 
                 GeoTagAlgorithm geoTagAlgorithm = new GeoTagAlgorithmImpl();
                 HashMap<TLogParser.Event, File> matchedPhotos = geoTagAlgorithm.match(events, photos);
 
                 if (!hasEnoughMemory(saveDir.getPath(), matchedPhotos.values())) {
-                    sendFailed(handler, callback, new Exception("Not enough memory left."));
+                    sendFailed(handler, callback, new OutOfMemoryError());
                     return;
                 }
 
                 HashMap<File, File> geoTaggedFiles = new HashMap<>();
                 HashMap<File, Exception> failedFiles = new HashMap<>();
 
-                for (TLogParser.Event event : matchedPhotos.keySet()) {
-                    File photo = matchedPhotos.get(event);
+                for (Map.Entry<TLogParser.Event, File> entry : matchedPhotos.entrySet()) {
+                    File photo = entry.getValue();
 
                     File newFile = new File(saveDir.getPath(), photo.getName());
                     try {
                         copyFile(photo.getPath(), newFile.getPath());
-                        updateExif(event, newFile);
+                        updateExif(entry.getKey(), newFile);
                         geoTaggedFiles.put(photo, newFile);
                     } catch (Exception e) {
-                        failedFiles.put(photo, new Exception());
+                        failedFiles.put(photo, e);
                     }
                 }
 
                 sendResult(handler, callback, geoTaggedFiles, failedFiles);
             }
         });
+    }
+
+    private static File getSaveDir(Context context) {
+        File dirs[] = context.getExternalFilesDirs(null);
+        File saveDir = context.getExternalFilesDir(null);
+        for (File dir : dirs) {
+            if (Environment.isExternalStorageRemovable(dir)) {
+                saveDir = dir;
+                break;
+            }
+        }
+        return saveDir;
     }
 
     private static boolean hasEnoughMemory(String filePath, Collection<File> photos) {
@@ -109,10 +116,8 @@ public class Geotag {
     }
 
     private static void copyFile(String inputPath, String outputPath) throws IOException {
-        InputStream in;
-        OutputStream out;
-        in = new FileInputStream(inputPath);
-        out = new FileOutputStream(outputPath);
+        InputStream in = new FileInputStream(inputPath);
+        OutputStream out = new FileOutputStream(outputPath);
 
         byte[] buffer = new byte[1024];
         int read;
@@ -138,7 +143,7 @@ public class Geotag {
         }
     }
 
-    private static void sendFailed(Handler handler, final GeoTagCallback geoTagCallback, final Exception exception) {
+    private static void sendFailed(Handler handler, final GeoTagCallback geoTagCallback, final Throwable exception) {
         if (geoTagCallback != null) {
             handler.post(new Runnable() {
                 @Override
@@ -156,15 +161,15 @@ public class Geotag {
         String alt = String.valueOf(msg.alt_msl);
 
         ExifInterface newExif = new ExifInterface(photoFile.getPath());
-        newExif.setAttribute(ExifInterface.TAG_GPS_LONGITUDE, convert(lng));
-        newExif.setAttribute(ExifInterface.TAG_GPS_LATITUDE, convert(lat));
+        newExif.setAttribute(ExifInterface.TAG_GPS_LONGITUDE, convertLatLngToDMS(lng));
+        newExif.setAttribute(ExifInterface.TAG_GPS_LATITUDE, convertLatLngToDMS(lat));
         newExif.setAttribute(ExifInterface.TAG_GPS_LATITUDE_REF, lat < 0 ? "S" : "N");
         newExif.setAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF, lng < 0 ? "W" : "E");
         newExif.setAttribute(ExifInterface.TAG_GPS_ALTITUDE, alt);
         newExif.saveAttributes();
     }
 
-    private static final String convert(double coord) {
+    private static final String convertLatLngToDMS(double coord) {
 
         coord = Math.abs(coord);
         int degree = (int) coord;
@@ -191,15 +196,16 @@ public class Geotag {
          * Callback for successful geotagging
          *
          * @param geoTaggedPhotos {@link HashMap<File, File>} map of files sent in to the geotagged files.
-         * @param failedFiles {@link HashMap<File, Exception>} map of files sent in to exception that occurred when geotagging.
+         * @param failedFiles     {@link HashMap<File, Exception>} map of files sent in to exception that occurred when geotagging.
          */
         void onResult(HashMap<File, File> geoTaggedPhotos, HashMap<File, Exception> failedFiles);
 
         /**
          * Callback for failure in geotagging
+         *
          * @param e {@link Exception}
          */
-        void onFailed(Exception e);
+        void onFailed(Throwable e);
     }
 
     private static class GeoTagAlgorithmImpl implements GeoTagAlgorithm {
@@ -210,19 +216,10 @@ public class Geotag {
             int eventsSize = events.size();
             int photosSize = photos.size();
 
-            if (photosSize > eventsSize) {
-                int j = photosSize - 1;
-                for (int i = eventsSize - 1; i >= 0; i--) {
-                    matchedMap.put(events.get(i), photos.get(j));
-                    j--;
-                }
-            } else {
-                int j = eventsSize - 1;
-                for (int i = photosSize - 1; i >= 0; i--) {
-                    matchedMap.put(events.get(j), photos.get(i));
-                    j--;
-                }
+            for (int i = eventsSize - 1, j = photosSize - 1; i >= 0 && j >= 0; i--, j--) {
+                matchedMap.put(events.get(i), photos.get(j));
             }
+
             return matchedMap;
         }
     }
