8fb7bdfdb6dfcd643bd691495066ad3980f95286
Chavi Weingarten <chavi@3drobotics.com>
2015-11-16 22:25:29 -0800

    Added unit tests

--- ServiceApp/test/java/com/o3dr/services/android/lib/drone/companion/solo/tlv/TLVMessageParserTest.java
+++ ServiceApp/test/java/com/o3dr/services/android/lib/drone/companion/solo/tlv/TLVMessageParserTest.java
@@ -5,7 +5,10 @@ import org.junit.Test;
 import java.nio.ByteBuffer;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for the TLVMessageParser class.
@@ -13,18 +16,28 @@ import static org.junit.Assert.*;
 public class TLVMessageParserTest {
 
     @Test
-    public void testParseTLVPacket() throws Exception {
-        List<TLVPacket> results = null;
+    public void testParseTLVPacket_nullParams() throws Exception {
+        List<TLVPacket> results;
 
         //Test for invalid parameters
         results = TLVMessageParser.parseTLVPacket((byte[]) null);
         assertNull(results);
+    }
+
+    @Test
+    public void testParseTLVPacket_emptyParams() {
+        List<TLVPacket> results;
 
         results = TLVMessageParser.parseTLVPacket(new byte[0]);
         assertNull(results);
+    }
+
+    @Test
+    public void testParseTLVPacket_singleMessage() {
+        List<TLVPacket> results;
 
         //Single message parsing test
-        SoloMessageLocation messageLoc = new SoloMessageLocation(5.3488066,-4.0499032, 10);
+        SoloMessageLocation messageLoc = new SoloMessageLocation(5.3488066, -4.0499032, 10);
         byte[] messageLocBytes = messageLoc.toBytes();
         results = TLVMessageParser.parseTLVPacket(messageLocBytes);
         assertNotNull(results);
@@ -38,13 +51,19 @@ public class TLVMessageParserTest {
         SoloMessageLocation castedResult = (SoloMessageLocation) resultPacket;
         assertTrue(castedResult.getCoordinate().equals(messageLoc.getCoordinate()));
 
+    }
+
+    @Test
+    public void testParseTLVPacket_multipleMessages() {
+        List<TLVPacket> results;
+        SoloMessageLocation messageLoc = new SoloMessageLocation(5.3488066, -4.0499032, 10);
 
         //Multiple message parsing test
         //1.
         ByteBuffer inputData = ByteBuffer.allocate(46);
         SoloMessageShotGetter shotGetter = new SoloMessageShotGetter(SoloMessageShot.SHOT_CABLECAM);
         TLVPacket[] inputPackets = {messageLoc, shotGetter};
-        for(TLVPacket packet: inputPackets){
+        for (TLVPacket packet : inputPackets) {
             inputData.put(packet.toBytes());
         }
         inputData.rewind();
@@ -54,15 +73,24 @@ public class TLVMessageParserTest {
         assertTrue(results.size() == inputPackets.length);
         assertTrue(inputData.remaining() == 6);
 
-        for(int i = 0; i < inputPackets.length; i++){
+        for (int i = 0; i < inputPackets.length; i++) {
             TLVPacket inputPacket = inputPackets[i];
             TLVPacket outputPacket = results.get(i);
             assertTrue(inputPacket.equals(outputPacket));
         }
 
+    }
+
+    @Test
+    public void testParseTLVPacket_multipleMessages2() {
+        List<TLVPacket> results;
+
         //2.
-        inputData = ByteBuffer.allocate(40);
-        for(TLVPacket packet: inputPackets){
+        SoloMessageShotGetter shotGetter = new SoloMessageShotGetter(SoloMessageShot.SHOT_CABLECAM);
+        SoloMessageLocation messageLoc = new SoloMessageLocation(5.3488066, -4.0499032, 10);
+        ByteBuffer inputData = ByteBuffer.allocate(40);
+        TLVPacket[] inputPackets = {messageLoc, shotGetter};
+        for (TLVPacket packet : inputPackets) {
             inputData.put(packet.toBytes());
         }
         inputData.rewind();
@@ -72,10 +100,120 @@ public class TLVMessageParserTest {
         assertTrue(results.size() == inputPackets.length);
         assertTrue(inputData.remaining() == 0);
 
-        for(int i = 0; i < inputPackets.length; i++){
+        for (int i = 0; i < inputPackets.length; i++) {
             TLVPacket inputPacket = inputPackets[i];
             TLVPacket outputPacket = results.get(i);
             assertTrue(inputPacket.equals(outputPacket));
         }
+    }
+
+    @Test
+    public void testParseTLVPacket_goProSetExtended() {
+        byte[] values = new byte[4];
+
+        SoloGoproSetExtendedRequest extendedRequest =
+            new SoloGoproSetExtendedRequest(SoloGoproConstants.CAPTURE_MODE, values);
+
+        TLVPacket[] inputPackets = {extendedRequest};
+
+        ByteBuffer inputData = ByteBuffer.allocate(14);
+        for (TLVPacket packet : inputPackets) {
+            inputData.put(packet.toBytes());
+        }
+        inputData.rewind();
+
+        List<TLVPacket> results = TLVMessageParser.parseTLVPacket(inputData);
+        assertNotNull(results);
+        assertEquals(inputPackets.length, results.size());
+        assertEquals(0, inputData.remaining());
+
+        assertEquals(extendedRequest, results.get(0));
+    }
+
+    @Test
+    public void testParseTLVPacket_goProSetRequest() {
+        SoloGoproSetRequest setRequest =
+            new SoloGoproSetRequest(SoloGoproConstants.CAPTURE_MODE, SoloGoproConstants.POWER_OFF);
+
+        TLVPacket[] inputPackets = {setRequest};
+
+        ByteBuffer inputData = ByteBuffer.allocate(12);
+        for (TLVPacket packet : inputPackets) {
+            inputData.put(packet.toBytes());
+        }
+        inputData.rewind();
+
+        List<TLVPacket> results = TLVMessageParser.parseTLVPacket(inputData);
+        assertNotNull(results);
+        assertEquals(inputPackets.length, results.size());
+        assertEquals(0, inputData.remaining());
+
+        assertEquals(setRequest, results.get(0));
+    }
+
+    @Test
+    public void testParseTLVPacket_goProRecord() {
+        SoloGoproRecord record =
+            new SoloGoproRecord(SoloGoproConstants.STOP_RECORDING);
+
+        TLVPacket[] inputPackets = {record};
+
+        ByteBuffer inputData = ByteBuffer.allocate(12);
+        for (TLVPacket packet : inputPackets) {
+            inputData.put(packet.toBytes());
+        }
+        inputData.rewind();
+
+        List<TLVPacket> results = TLVMessageParser.parseTLVPacket(inputData);
+        assertNotNull(results);
+        assertEquals(0, inputData.remaining());
+
+        assertEquals(record, results.get(0));
+    }
+
+    @Test
+    public void testParseTLVPacket_goProState() {
+        SoloGoproState state =
+            new SoloGoproState(
+                (byte) 1, (byte) 1, (byte) 1, (byte) 1, (byte) 1, (byte) 1, (byte) 1, (byte) 1,
+                (byte) 1, (byte) 1, (byte) 1, (byte) 1, (byte) 1, (byte) 1, (byte) 1, (byte) 1,
+                (byte) 1, (byte) 1, (byte) 1, (byte) 1, (byte) 1, (byte) 1, (byte) 1, (byte) 1,
+                (byte) 1, (byte) 1, (byte) 1, (byte) 1, (byte) 1, (byte) 1, (byte) 1);
+
+        TLVPacket[] inputPackets = {state};
+
+        ByteBuffer inputData = ByteBuffer.allocate(44);
+        for (TLVPacket packet : inputPackets) {
+            inputData.put(packet.toBytes());
+        }
+        inputData.rewind();
+
+        List<TLVPacket> results = TLVMessageParser.parseTLVPacket(inputData);
+        assertNotNull(results);
+        assertEquals(inputPackets.length, results.size());
+        assertEquals(0, inputData.remaining());
+
+        assertEquals(state, results.get(0));
+    }
+
+    @Test
+    public void testParseTLVPacket_goProRequestState() {
+        SoloGoproRequestState requestState =
+            new SoloGoproRequestState();
+
+        TLVPacket[] inputPackets = {requestState};
+
+        ByteBuffer inputData = ByteBuffer.allocate(8);
+        for (TLVPacket packet : inputPackets) {
+            inputData.put(packet.toBytes());
+        }
+        inputData.rewind();
+
+        List<TLVPacket> results = TLVMessageParser.parseTLVPacket(inputData);
+        assertNotNull(results);
+        assertEquals(inputPackets.length, results.size());
+        assertEquals(0, inputData.remaining());
+
+        assertEquals(requestState, results.get(0));
     }
 }
