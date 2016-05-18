1f5141d5e01123b24a4d4c47326e92e22dbf62d9
ne0fhyk <fhuyakou@gmail.com>
2016-05-18 01:49:55 -0700

    Fix package name

--- ClientLib/src/main/java/com/o3dr/services/android/lib/util/Utils.java
+++ ClientLib/src/main/java/com/o3dr/services/android/lib/util/Utils.java
@@ -5,5 +5,5 @@ package com.o3dr.services.android.lib.util;
  */
 public class Utils {
 
-    public static final String PACKAGE_NAME = "org.droidplanner.services.android";
+    public static final String PACKAGE_NAME = "com.o3dr.services.android";
 }
