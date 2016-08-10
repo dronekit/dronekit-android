5d43a57d17d35c5ff5e277b7a6faac3916fe24b2
fhuya <fhuyakou@gmail.com>
2016-08-10 10:02:32 -0700

    Set correctly the pitch value for the takeoff mission item

--- ClientLib/src/main/java/org/droidplanner/services/android/impl/utils/ProxyUtils.java
+++ ClientLib/src/main/java/org/droidplanner/services/android/impl/utils/ProxyUtils.java
@@ -2,28 +2,6 @@ package org.droidplanner.services.android.impl.utils;
 
 import android.util.Log;
 
-import org.droidplanner.services.android.impl.core.mission.Mission;
-import org.droidplanner.services.android.impl.core.mission.MissionItemImpl;
-import org.droidplanner.services.android.impl.core.mission.commands.CameraTriggerImpl;
-import org.droidplanner.services.android.impl.core.mission.commands.ChangeSpeedImpl;
-import org.droidplanner.services.android.impl.core.mission.commands.ConditionYawImpl;
-import org.droidplanner.services.android.impl.core.mission.commands.DoJumpImpl;
-import org.droidplanner.services.android.impl.core.mission.commands.EpmGripperImpl;
-import org.droidplanner.services.android.impl.core.mission.commands.ReturnToHomeImpl;
-import org.droidplanner.services.android.impl.core.mission.commands.SetRelayImpl;
-import org.droidplanner.services.android.impl.core.mission.commands.SetServoImpl;
-import org.droidplanner.services.android.impl.core.mission.commands.TakeoffImpl;
-import org.droidplanner.services.android.impl.core.mission.survey.SplineSurveyImpl;
-import org.droidplanner.services.android.impl.core.mission.survey.SurveyImpl;
-import org.droidplanner.services.android.impl.core.mission.waypoints.CircleImpl;
-import org.droidplanner.services.android.impl.core.mission.waypoints.DoLandStartImpl;
-import org.droidplanner.services.android.impl.core.mission.waypoints.LandImpl;
-import org.droidplanner.services.android.impl.core.mission.waypoints.RegionOfInterestImpl;
-import org.droidplanner.services.android.impl.core.mission.waypoints.SplineWaypointImpl;
-import org.droidplanner.services.android.impl.core.mission.waypoints.StructureScannerImpl;
-import org.droidplanner.services.android.impl.core.mission.waypoints.WaypointImpl;
-import org.droidplanner.services.android.impl.core.survey.CameraInfo;
-import org.droidplanner.services.android.impl.core.survey.SurveyData;
 import com.o3dr.services.android.lib.coordinate.LatLongAlt;
 import com.o3dr.services.android.lib.drone.mission.item.MissionItem;
 import com.o3dr.services.android.lib.drone.mission.item.command.CameraTrigger;
@@ -47,6 +25,29 @@ import com.o3dr.services.android.lib.drone.mission.item.spatial.Land;
 import com.o3dr.services.android.lib.drone.mission.item.spatial.RegionOfInterest;
 import com.o3dr.services.android.lib.drone.mission.item.spatial.SplineWaypoint;
 import com.o3dr.services.android.lib.drone.mission.item.spatial.Waypoint;
+
+import org.droidplanner.services.android.impl.core.mission.Mission;
+import org.droidplanner.services.android.impl.core.mission.MissionItemImpl;
+import org.droidplanner.services.android.impl.core.mission.commands.CameraTriggerImpl;
+import org.droidplanner.services.android.impl.core.mission.commands.ChangeSpeedImpl;
+import org.droidplanner.services.android.impl.core.mission.commands.ConditionYawImpl;
+import org.droidplanner.services.android.impl.core.mission.commands.DoJumpImpl;
+import org.droidplanner.services.android.impl.core.mission.commands.EpmGripperImpl;
+import org.droidplanner.services.android.impl.core.mission.commands.ReturnToHomeImpl;
+import org.droidplanner.services.android.impl.core.mission.commands.SetRelayImpl;
+import org.droidplanner.services.android.impl.core.mission.commands.SetServoImpl;
+import org.droidplanner.services.android.impl.core.mission.commands.TakeoffImpl;
+import org.droidplanner.services.android.impl.core.mission.survey.SplineSurveyImpl;
+import org.droidplanner.services.android.impl.core.mission.survey.SurveyImpl;
+import org.droidplanner.services.android.impl.core.mission.waypoints.CircleImpl;
+import org.droidplanner.services.android.impl.core.mission.waypoints.DoLandStartImpl;
+import org.droidplanner.services.android.impl.core.mission.waypoints.LandImpl;
+import org.droidplanner.services.android.impl.core.mission.waypoints.RegionOfInterestImpl;
+import org.droidplanner.services.android.impl.core.mission.waypoints.SplineWaypointImpl;
+import org.droidplanner.services.android.impl.core.mission.waypoints.StructureScannerImpl;
+import org.droidplanner.services.android.impl.core.mission.waypoints.WaypointImpl;
+import org.droidplanner.services.android.impl.core.survey.CameraInfo;
+import org.droidplanner.services.android.impl.core.survey.SurveyData;
 
 /**
  * Created by fhuya on 11/10/14.
@@ -140,7 +141,8 @@ public class ProxyUtils {
             case TAKEOFF: {
                 Takeoff proxy = (Takeoff) proxyItem;
 
-                TakeoffImpl temp = new TakeoffImpl(mission, (proxy.getTakeoffAltitude()));
+                TakeoffImpl temp = new TakeoffImpl(mission, proxy.getTakeoffAltitude(),
+                    proxy.getTakeoffPitch());
 
                 missionItemImpl = temp;
                 break;
